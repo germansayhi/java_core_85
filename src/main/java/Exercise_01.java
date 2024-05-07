71a5b9534ebb3dda55e4dfa10097919019f5f79f
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-05-07 21:13:54 +0700

    btvn

--- src/main/java/Exercise_01.java
+++ src/main/java/Exercise_01.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Exercise_01 {
+public class Exercise_01 {
+    void Question01() {
+        Account account = new Account();
+        if (account.Department == null) {
+            System.out.println("Nhân viên này chưa có phòng ban");
+        } else {
+            System.out.println("Phòng ban của nhân viên này là: " + account.Department.name);
+        }
+    }
+
+    void Question03() {
+        Department department = new Department();
+        department.name = "CEO";
+
+        Account account = new Account();
+        account.Department = department;
+
+        String mess = (account.Department == null)
+                ? "Nhân viên này chưa có phòng ban"
+                : "Phòng ban của nhân viên này là: " + account.Department.name;
+        System.out.println(mess);
+
+    }
+
+    void Question04() {
+        Position position10 = new Position();
+        position10.Position = PositionName.Dev;
+        Position position11 = new Position();
+        position10.Position = PositionName.PM;
+
+        Account account10 = new Account();
+        account10.Position = position10;
+
+        String cu_phap = (account10.Position == position10)
+
+                ? "Đây là Developer"
+                : "Người này không phải là Developer";
+        System.out.println(cu_phap);
+
+    }
+
+    void Question07() {
+        Position position10 = new Position();
+        position10.Position = PositionName.PM;
+
+        Account account10 = new Account();
+        account10.Position = position10;
+        System.out.println(account10.Position.Position.name());
+        switch (account10.Position.Position.name()) {
+            case "Dev":
+                System.out.println("Đây là Developer");
+                break;
+            default:
+                System.out.println("Người này không phải là Developer");
+                break;
+
+
+        }
+    }
 }
+
+
--- src/main/java/Program01.java
+++ src/main/java/Program01.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Program01 {
+public class Program01 {
+    public static void main(String[] args) {
+        Exercise_01 ex1 = new Exercise_01();
+        ex1.Question01();
+        Exercise_01 ex3 = new Exercise_01();
+        ex3.Question03();
+        Exercise_01 ex04 = new Exercise_01();
+        ex04.Question04();
+        Exercise_01 ex013 = new Exercise_01();
+        System.out.println("................");
+        int i;
+        for (i = 0; i <= 20;i=i+2 ) {
+            System.out.println("i=" + i);
+        }
+        Exercise_01 ex07 = new Exercise_01();
+        ex07.Question07();
+
+
+
+    }
+
 }
