efdce168665b8cba23537cd3b581109cb4130eda
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-05-22 20:16:38 +0700

    bai9

--- src/main/java/StaticDemo.java
+++ src/main/java/StaticDemo.java
@@ -3,6 +3,9 @@ public class StaticDemo {
         Student s1 = new Student("Duc", 20);
         Student s2 = new Student("Khoa", 22);
 
+        s1.count =10;
+        System.out.println(Student.count);
+
         // Truy cap thong qua class
         int max = Student.max(100, 32);
         System.out.println("max = " + max);
