72837934afc83651cb61cecf301772067e651b4f
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-05-26 20:45:58 +0700

    bai 10

---
+++ src/main/java/backend/Excercise01.java
@@ -0,0 +1,11 @@
+package backend;
+
+import entity.Student;
+
+public class Excercise01 {
+    public static void Question01(){
+        Student[] students = new Student[3];
+        students[0] = new Student("A");
+
+    }
+}
